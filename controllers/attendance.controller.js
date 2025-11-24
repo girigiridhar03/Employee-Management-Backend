77f48ec21bdeb24ed2fbeb61465af9e6bfe14bac
Giridhar D <girigiridhar0603@gmail.com>
2025-11-24 08:08:44 +0530

    added new routes for attendance

--- controllers/attendance.controller.js
+++ controllers/attendance.controller.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import { response } from "../utils/response.js";
 import Attendance from "../models/attendance.model.js";
+import Employee from "../models/employee.model.js";
 
 export const attendanceToggle = async (req, res) => {
   try {
@@ -48,5 +49,58 @@ export const attendanceToggle = async (req, res) => {
     });
   } catch (error) {
     response(res, 500, "Internal Server error");
+  }
+};
+
+export const getAttendanceForAllEmployees = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const size = parseInt(req.query.size) || 10;
+    const skip = (page - 1) * size;
+    const totalEmployees = await Employee.countDocuments();
+    const totalPage = Math.ceil(totalEmployees / size);
+
+    const allEmployees = await Employee.find({ status: "active" })
+      .skip(skip)
+      .limit(size)
+      .select("username employeeId email designation")
+      .populate("reportingTo", "username employeeId email designation");
+
+    const today = dayjs().format("YYYY-MM-DD");
+    const attendanceRecord = await Attendance.find({ date: today });
+
+    const attendanceMap = {};
+
+    attendanceRecord.forEach((att) => {
+      attendanceMap[String(att.employeeDetails)] = att;
+    });
+
+    const enRichedEmployees = allEmployees.map((emp) => {
+      const att = attendanceMap[String(emp._id)];
+      if (!att) {
+        return {
+          ...emp._doc,
+          status: "yet-to-checkin",
+        };
+      }
+
+      return {
+        ...emp._doc,
+        status: att.status,
+        checkIn: att.checkIn,
+        checkOut: att.checkOut,
+        totalHours: att.totalHours,
+      };
+    });
+
+    response(res, 200, "Attendance Analytics Fetched successfully", {
+      attendance: enRichedEmployees,
+      totalPage,
+      page,
+      size,
+      totalEmployees,
+    });
+  } catch (error) {
+    response(res, 500, "Internal server error");
   }
 };
--- routes/attendance.routes.js
+++ routes/attendance.routes.js
@@ -1,9 +1,19 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/auth.middlware.js";
-import { attendanceToggle } from "../controllers/attendance.controller.js";
+import {
+  attendanceToggle,
+  getAttendanceForAllEmployees,
+} from "../controllers/attendance.controller.js";
+import adminHrAndLoggedUser from "../middlewares/adminAndHr.middleware.js";
 
 const attendanceRouter = express.Router();
 
 attendanceRouter.post("/attendance/toggle", authMiddleware, attendanceToggle);
+attendanceRouter.get(
+  "/attendance/today",
+  authMiddleware,
+  adminHrAndLoggedUser,
+  getAttendanceForAllEmployees
+);
 
 export default attendanceRouter;
